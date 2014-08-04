bcd38f751f01846c88c516657e7ea175455b8bce
Christopher Vogt <christopher.vogt@epfl.ch>
2014-08-04 04:02:04 -0400

    Release 2.1.0

--- project/Build.scala
+++ project/Build.scala
@@ -35,7 +35,7 @@ object stagedBuild extends Build {
   val sharedSettings = Project.defaultSettings ++ Seq(
     scalaVersion := "2.10.3",
     libraryDependencies ++= List(
-      "com.typesafe.slick" %% "slick" % "2.1.0-RC3",
+      "com.typesafe.slick" %% "slick" % "2.1.0",
       "org.slf4j" % "slf4j-nop" % "1.6.4",
       "com.h2database" % "h2" % "1.3.170"
     )
